c184eca3447ea2d56656a804e5547e2899b7fc9d
Karen Franco <karen_franco23@hotmail.com>
2023-09-13 15:59:02 -0300

    feat: Adiciona CRUD de usuário

---
+++ src/Yummi.Backend/Yummi.Backend/Data/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using Yummi.Backend.Models;
+
+namespace Yummi.Backend.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IMongoCollection<User> _usersCollection;
+
+        public UserRepository(IOptions<DatabaseConfiguration> databaseConfiguration)
+        {
+            var mongoClient = new MongoClient(databaseConfiguration.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(databaseConfiguration.Value.DatabaseName);
+            _usersCollection = mongoDatabase.GetCollection<User>("User");
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _usersCollection.InsertOneAsync(user);
+        }
+
+        public async Task DeleteUserAsync(string id)
+        {
+            var user = await _usersCollection.FindAsync(x => x.Id == id).Result.FirstAsync();
+            user.Active = false;
+            await _usersCollection.ReplaceOneAsync(x => x.Id == id, user);
+        }
+
+        public IMongoQueryable<User> GetAllUsers()
+        {
+            return _usersCollection.AsQueryable();
+        }
+
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            return await _usersCollection.FindAsync(x => x.Id == id).Result.FirstAsync();
+        }
+
+        public async Task<User> LoginAsync(string email, string password)
+        {
+            return await _usersCollection.FindAsync(x => x.Email == email && x.Password == password).Result.FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            await _usersCollection.ReplaceOneAsync(x => x.Id == user.Id, user);
+        }
+    }
+}
---
+++ src/Yummi.Backend/Yummi.Backend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Yummi.Backend.Models
+{
+    public class User : Entity
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
