d02e90ba66fbbd4f53f7c10643fd5bb5daab7dfd
pedrovitorrs <pedro442.rodrigues@gmail.com>
2023-09-14 01:06:26 -0300

    feat: adicionando dockerfile

--- src/Yummi.Backend/Yummi.Backend/Program.cs
+++ src/Yummi.Backend/Yummi.Backend/Program.cs
@@ -65,11 +65,11 @@ builder.Services.AddAuthentication(option =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseAuthentication();
 app.UseAuthorization();
