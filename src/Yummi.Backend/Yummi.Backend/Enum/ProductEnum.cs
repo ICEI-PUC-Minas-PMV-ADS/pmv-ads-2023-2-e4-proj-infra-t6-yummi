2df40787441a2399f8de252be1ad6b28c02cae67
Jvicentemelo <52583833+Jvicentemelo@users.noreply.github.com>
2023-09-27 14:19:30 -0300

    Criados Dto e Enum para products

---
+++ src/Yummi.Backend/Yummi.Backend/Enum/ProductEnum.cs
@@ -0,0 +1,77 @@
+namespace Yummi.Backend.Enum
+{
+    public enum TipoRefeicao
+    {
+        CafeDaManha,
+        Almoco,
+        Jantar,
+        Sobremesas,
+        Bebidas,
+        HappyHour,
+        MenuInfantil
+    }
+
+    public enum TipoCozinha
+    {
+        Brasileira,
+        Italiana,
+        Chinesa,
+        Japonesa,
+        Indiana,
+        Mexicana,
+        Mediterranea,
+        Vegetariana,
+        Churrascaria
+    }
+
+    public enum CategoriaPrato
+    {
+        Entradas,
+        PratosPrincipais,
+        Aperitivos,
+        Saladas,
+        Sanduiches,
+        Massas,
+        Pizza,
+        Sopas
+    }
+
+    public enum TipoBebida
+    {
+        Refrigerantes,
+        SucosNaturais,
+        Cafes,
+        Chas,
+        Vinhos,
+        Cervejas,
+        Coqueteis,
+        BebidasAlcoolicas
+    }
+
+    public enum EstiloCulinaria
+    {
+        Gourmet,
+        FastFood,
+        ComidaDeRua,
+        Saudavel,
+        Caseira,
+        Fusion
+    }
+
+    public enum RestricaoAlimentar
+    {
+        SemGluten,
+        SemLactose,
+        Vegan,
+        Vegetariano,
+        SemNozes,
+        BaixoTeorDeSodio
+    }
+
+    public enum Temporada
+    {
+        EspecialDeVerao,
+        EspecialDeInverno,
+        PromocoesDeFimDeAno
+    }
+}
