842396f29c1bd333f9a68b34f2d095f4d9638106
Jvicentemelo <52583833+Jvicentemelo@users.noreply.github.com>
2023-10-14 13:17:38 -0300

    ProductUpdateDto

---
+++ src/Yummi.Backend/Yummi.Backend/Dtos/Products/ProductUpdateDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Yummi.Backend.Enum;
+
+namespace Yummi.Backend.Dtos.Products
+{
+    public class ProductUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        public string Nome { get; set; }
+        
+        public string Descricao { get; set; }
+        
+        public decimal Preco { get; set; }
+        
+        public TipoRefeicao TipoRefeicao { get; set; }
+
+        public TipoCozinha TipoCozinha { get; set; }
+
+        public Models.Categoria Categoria { get; set; }
+
+        public TipoBebida TipoBebida { get; set; }
+
+        public EstiloCulinaria Estilo { get; set; }
+
+        public RestricaoAlimentar? Restricao { get; set; }
+
+        public Temporada? Temporada { get; set; }
+    }   
+}
