06bc5bf0492930ce8ebfaac25f57b52fb4121ce8
Jvicentemelo <52583833+Jvicentemelo@users.noreply.github.com>
2023-10-20 20:28:05 -0300

    Reset andriod

---
+++ src/YummiAndroid/App.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import axios from 'axios';
+
+const ProductScreen = () => {
+  const [products, setProducts] = useState([]);
+
+
+  const fetchProducts = () => {
+    axios.get('http://localhost:5000/api/products/{id}')
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <View>
+      <Text>Lista de Produtos:</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Nome: {item.nome}</Text>
+            <Text>Descrição: {item.descricao}</Text>
+            <Text>Preço: R$ {item.preco.toFixed(2)}</Text>
+            {/* Exiba outras informações do produto aqui */}
+          </View>
+        )}
+      />
+
+    </View>
+  );
+};
+
+export default ProductScreen;
